3500266579f92e5a424ff5cb3598cd79bf30c49a
Sankha-Suvra <sankhasuvhraghatak@gmail.com>
2024-10-01 02:01:01 +0530

    added data table and crud feature

--- client/src/components/container.tsx
+++ client/src/components/container.tsx
@@ -1,11 +1,35 @@
 import * as React from 'react';
 import FormContainer from './FormContainer';
+import { UrlData } from '../interface/UrlData';
+import axios from 'axios';
+import { serverUrl } from '../helpers/Constants';
+import DataTable from './DataTable';
 
 interface IContainerProps {
 }
 
 const Container: React.FunctionComponent<IContainerProps> = () => {
-  return <><FormContainer></FormContainer></>;
+  const[data, setData] = React.useState<UrlData[]>([])
+  const[reload, setReload] = React.useState<boolean>(false)
+
+  const updateReloadState = ():void =>{
+    setReload(true)
+  }
+  const fetchTableData = async () => {
+    const response = await axios.get(`${serverUrl}/shortUrl`);
+    console.log("the response from server is", response);
+    setData(response.data)   
+    setReload(false)
+    
+  }
+  React.useEffect(()=>{
+    fetchTableData()
+  },[reload])
+  return (
+  <>
+    <FormContainer updateReloadState={updateReloadState}/>
+    <DataTable updateReloadState={updateReloadState} data={data}/>
+  </>);
 };
 
 export default Container;
