38f1f31565069cf6ce916d8dd5dac22267ce7805
Sankha-Suvra <sankhasuvhraghatak@gmail.com>
2025-01-20 14:14:43 +0530

    changed server url and cors origin

--- client/src/helpers/Constants.ts
+++ client/src/helpers/Constants.ts
@@ -1,2 +1,2 @@
-export const serverUrl = import.meta.env.VITE_SERVER_URL || "http://13.203.79.156/api"
+export const serverUrl = import.meta.env.VITE_SERVER_URL || "http://65.0.122.38/api"
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin: "*",
+    origin: "https://hilarious-pie-38e44f.netlify.app", //changed from * to specific
     credentials: true,
 }))
 
